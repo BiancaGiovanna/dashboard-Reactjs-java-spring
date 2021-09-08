1e03914f53861895d676d2c1bb72311af574202e
BiancaGiovanna <biancagothicg@gmail.com>
2021-09-08 04:03:21 -0300

    criando o projeto

---
+++ backend/src/main/java/com/dashboardbianca/dashboardvendas/DashboardvendasApplication.java
@@ -0,0 +1,13 @@
+package com.dashboardbianca.dashboardvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dashboardbianca/dashboardvendas/DashboardvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboardbianca.dashboardvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
